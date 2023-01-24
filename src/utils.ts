13687511a19aa2e9a8bacb02cd4c9bbbebb46f46
Paulo <mfjpaulo@gmail.com>
2023-01-24 13:19:16 +0000

    Fix Hook Firing More Than Once (#6)
    * Fix removeEventListener not working
    
    * Rename
    
    * Bump lint

--- src/utils.ts
+++ src/utils.ts
@@ -1,19 +1,5 @@
-import { Config } from "./types";
+import { Target } from "./types";
 
-export const getEventTargetFromConfig = (config?: Config) => {
-  if (config?.target) {
-    const { target } = config;
-
-    // If the target is a ref...
-    if ("current" in target) {
-      // ...and the ref has a value, return it
-      if (target.current) return target.current;
-    }
-
-    // Otherwise, return the target
-    else return target;
-  }
-
-  // If no target is specified, or it's null, return window
-  return window;
+export const getEventTargetFromTarget = (target?: Target) => {
+  return (target && "current" in target ? target.current : target) || window;
 };
